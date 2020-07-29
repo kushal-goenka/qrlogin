5bdb2f2dd46cbcb5d3539ede7bec8460456799d6
Kushal-Goenka <kushalgoenka97@gmail.com>
2020-07-29 10:02:56 -0500

    add basic files

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import qrcode
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   return "Hello World"
+
+@app.route('/qrcode')
+def qrcode():
+   return "Another page"
+
+
+if __name__ == '__main__':
+   app.run(debug=True,host='0.0.0.0')
